e8ef911a8520e5b86f5a8b56e71d6358fdab560a
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-29 19:33:52 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,4 +1,11 @@
 import pandas as pd
 import csv
 
+st.title('Análise do Instagram de João Campos')
+
 arquivo = open('JoaoInsta.csv')
+
+st.write("AS 10 PUBLICAÇÕES MAIS ENGAJADAS")
+
+likes = df[['Link', 'Post Created Date', 'Likes']]
+likes.sort_values(by='Likes', ascending=False).head(10)
