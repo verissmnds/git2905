eb48b2746402c85046020aca66dc8af39183d145
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-29 19:46:08 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -9,5 +9,5 @@ st.title('Análise do Instagram de João Campos')
 
 st.write("AS 10 PUBLICAÇÕES MAIS ENGAJADAS")
 
-likes = df[['Link', 'Post Created Date', 'Likes']]
+likes = arquivo[['Link', 'Post Created Date', 'Likes']]
 likes.sort_values(by='Likes', ascending=False).head(10)
