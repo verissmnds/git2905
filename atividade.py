13991a9bd899086154fccc7093558353e2865c6d
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-29 21:47:32 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st 
 
 st.title('Análise do Instagram de João Campos')
-st.write('Analisando o engajamento da Campanha Permanente do prefeito João Campos')
+st.write('Analisando o engajamento da Campanha Permanente do prefeito João Campos e o seu impacto na opinião pública.')
 
 dados = pd.read_csv('Joao.csv.csv')
 st.dataframe(dados)
