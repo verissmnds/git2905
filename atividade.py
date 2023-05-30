a6f031d2a3bb2121df6b297d558c8741e93fedfe
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-29 21:28:34 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -4,8 +4,5 @@ import streamlit as st
 st.title('Análise do Instagram de João Campos')
 st.write("AS 10 PUBLICAÇÕES COM MAIOR ENGAJAMENTO")
 
-df['Total Interactions'] = df['Total Interactions'].apply(lambda x: x.replace(',', ''))
-df['Total Interactions'] = df['Total Interactions'].astype(int)
-
 interactions = df[['Link', 'Post Created Date', 'Total Interactions']]
 interactions.sort_values(by='Total Interactions', ascending=False).head(10)
