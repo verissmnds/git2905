6da81d0bbbd4f040b3c45ba72ee9ef7ab4739898
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-29 21:32:23 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import streamlit as st
+import streamlit as st 
 
-st.title('Análise do Instagram de João Campos')
-st.write("AS 10 PUBLICAÇÕES COM MAIOR ENGAJAMENTO")
-
-likes = df[['Link', 'Post Created Date', 'Likes']]
-likes.sort_values(by='Likes', ascending=False).head(10)
+st.title('CSV Viewer')
+dados = pd.read_csv('dados.csv')
+st.dataframe(dados)
