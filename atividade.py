00b6dbbcf43ee4968ecebdc17ea35bcdcbbef611
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-29 21:34:03 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st 
 
-st.title('CSV Viewer')
-dados = pd.read_csv('dados.csv')
+st.title('Análise do Instagram de João Campos')
+dados = pd.read_csv('Joao.csv.csv')
 st.dataframe(dados)
